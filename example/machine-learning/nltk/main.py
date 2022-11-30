7df0f5008c183e09c08298a2501160d8756ef369
wuheping <18810027915@163.com>
2022-11-30 21:49:13 +0800

    8008

--- example/machine-learning/nltk/main.py
+++ example/machine-learning/nltk/main.py
@@ -1,4 +1,7 @@
 '''
 一个自然语言处理的第三方库, NLP领域中常用, 可建立词袋模型（单词计数）,
 支持词频分析（单词出现次数）、模式识别、关联分析、情感分析（词频分析+度量指标）、可视化（+matploylib做分析图）等
+python3.8 -m pip install nltk -i https://mirrors.aliyun.com/pypi/simple
 '''
+
+import nltk
